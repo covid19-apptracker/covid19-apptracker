2ca6e0c97f9664a9f5dcffbf3cfec2a221bc4cb3
zach-anderson <zachsrad@gmail.com>
2020-04-15 13:36:37 -0700

    NavBar.js Updates
    Fixing up the NavBar and adding in proper toggle functionality for small screen sizes

--- src/NavBar.js
+++ src/NavBar.js
@@ -2,31 +2,48 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 
 class NavBar extends Component{
-    state = { showing:true };
+    state = {
+        showing: "text-sm sm:flex-grow antialiased hidden sm:block" 
+    };
+
+    boxClick = (e) => {
+        let stateUpdate = ((this.state.showing === "text-sm sm:flex-grow antialiased sm:block") ? "text-sm sm:flex-grow antialiased hidden sm:block" : "text-sm sm:flex-grow antialiased sm:block");
+        this.setState({
+            showing: stateUpdate
+        })
+        console.log('current state is: ' + this.state.showing);
+        console.log('made it into boxClick, stateUpdate is now: ' + stateUpdate);
+    }
+
 
     render() {
         function toggle() {
-            var x = document.getElementById("myLinks");
-                if (x.style.display === "block") {
-                    x.style.display = "none";
+                if (this.state.showing === "sm:block") {
+                    this.state.showing = "sm:none";
                 } else {
-                    x.style.display = "block";
+                    this.state.showing = "sm:block";
                 }
             }
+
+
+
         return (
         < nav className = "container max-w-screen-md mx-auto flex items-center justify-between flex-wrap bg-white p-6" >
             < div className = "flex items-center flex-shrink-0 text-black mr-6" >
-                <img className="fill-current h-8 w-8 mr-2" width="54" height="54" src={logo} />
+                <img className="fill-current h-8 w-8 mr-2 transition-all duration-300 ease-in-out transform hover:scale-125" width="54" height="54" src={logo} />
             </ div>
             <div className="block sm:hidden"> 
-                <button onClick={toggle} id="my-nav-menu" className="flex items-center px-3 py-2 border rounded text-black border-black hover:bg-black hover:text-white hover:border-black">
+                <button onClick={this.boxClick} id="my-nav-menu" className="flex items-center px-3 py-2 border rounded text-black border-black hover:bg-black hover:text-white hover:border-black">
                     <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
                 </button>
             </div>
-            < div className = "w-full float-right sm:flex sm:items-center sm:w-auto hidden md:block">
-                <div id="myLinks" className="text-sm lg:flex-grow antialiased">
-                   <a href="#responsive-header" class="block mt-4 sm:inline-block sm:mt-0 text-black hover:text-gray-500 mr-4">
-                        About the Project
+            < div className = "w-full float-right sm:flex sm:items-center sm:w-auto sm:block" >
+                <div id="myLinks" className={this.state.showing}>
+                   < a href = "#responsive-header" class = "block mt-4 sm:inline-block sm:mt-0 text-black hover:text-indigo-500 mr-4 transition-all duration-200 ease-in-out transform hover:-translate-y-1" >
+                        Home
+                    </a>
+                    < a className = "block mt-4 sm:inline-block sm:mt-0 text-black hover:text-indigo-500 transition-all duration-200 ease-in-out transform hover:-translate-y-1" >
+                        About
                     </a>
                 </div>
             </div>
